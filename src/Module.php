7ed3ea3699ceeb8a259a589478e1819d0ecfa53b
Maslakov Alexander <jmas.ukraine@gmail.com>
2017-04-29 23:19:30 +0300

    Init

---
+++ src/Module.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Flexo\Module\Example;
+
+class Module extends \Flexo\Core\Module
+{
+    public function getRootPath()
+    {
+        return dirname(__FILE__) . DIRECTORY_SEPARATOR . '..';
+    }
+
+    public function onRegister()
+    {
+        $this->app->get('/example', function($request, $response)
+        {
+            return $this->view->render($response, 'example/home.twig');
+        });
+    }
+}
